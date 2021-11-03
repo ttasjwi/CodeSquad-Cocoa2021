916664b12c6045d13fa555fe4d92491d39618ec5
ttasjwi <ttasjwi920@gmail.com>
2021-11-03 10:04:45 +0900

    rdmMember : Main, Prompt, README.md 생성

---
+++ src/rdmMember/Main.java
@@ -0,0 +1,7 @@
+package rdmMember;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/rdmMember/Prompt.java
@@ -0,0 +1,7 @@
+package rdmMember;
+
+public class Prompt {
+    void run() {
+
+    }
+}
