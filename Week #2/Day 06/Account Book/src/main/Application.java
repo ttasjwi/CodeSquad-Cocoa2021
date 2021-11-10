8e7d7c119b16ed7720ae2a9947e3e567d4b03703
ttasjwi <ttasjwi920@gmail.com>
2021-11-10 11:50:11 +0900

    Application : 사용자 입력 및 유효성 검사 기능 추가, run() 메서드 재귀메서드화

--- Week #2/Day 06/Account Book/src/main/Application.java
+++ Week #2/Day 06/Account Book/src/main/Application.java
@@ -1,6 +1,7 @@
 package main;
 
 import util.Inputter;
+
 import java.util.Scanner;
 
 public class Application {
@@ -19,26 +20,25 @@ public class Application {
         //* Record들을 읽어오고, AccountBooks에 추가한다.
     }
 
-    //TODO : Application 실행
-    public void run() {
-        System.out.println(START_MENU_MSG);
+    // 홈화면. 다음 창의 작업이 끝나면, 종료 명령어를 제외한 상황일 때 다시 run() 메서드를 호출함.
+    public boolean run() {
+        System.out.print(START_MENU_MSG);
         String command = inputCommand();
-        if (command.equals("q")) {
-            System.out.println("가계부 어플리케이션을 종료합니다. 바이바이~");
-            return;
+        switch (command) {
+            case "1":
+                System.out.println("로그인창으로 갈거야");
+                break;
+            case "2":
+                System.out.println("회원가입 창으로 갈거야");
+                break;
+            case "q":
+                System.out.println("가계부 어플리케이션을 종료합니다. 바이바이~");
+                return true;
         }
+        return run();
+    }
 
-        /*
-        0. 홈 화면을 띄운다.
-        1. 사용자로부터 입력을 받는다. (로그인, 회원가입, 종료)
-        2. 사용자로부터 입력을 받은 명령을 수행한다.
-            만약 명령이 정해진 명령이 유효하지 않으면 다시 입력받도록 돌아간다.
-         */
-        System.out.println("뭔가 유효한 값이 제대로 넘어왔나보군! 예이~");
-        }
-
-
-    //TODO : 사용자로부터 명령을 입력받아 반환하도록 함.
+    // 사용자로부터 명령을 입력받아 반환하도록 한다.
     private String inputCommand() {
 
         System.out.println("값을 입력해주세요... (1, 2, q) >  ");
@@ -55,11 +55,15 @@ public class Application {
         return input;
     }
 
+    // 사용자로부터 입력받은 홈화면 메뉴 선택의 유효성 체크
     private void inputValidCheck(String input) {
-        switch(input) {
-            case "1" : case "2" : case "q" :
+        switch (input) {
+            case "1":
+            case "2":
+            case "q":
                 return;
-            default : break;
+            default:
+                break;
         }
         throw new IllegalArgumentException("홈화면 입력 오류 : 유효한 입력값은 1, 2, q 입니다.");
     }
