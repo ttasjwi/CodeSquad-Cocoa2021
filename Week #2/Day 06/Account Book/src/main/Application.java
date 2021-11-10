f0c5936acd12ae5c6f50b394721a6abbd281fa31
ttasjwi <ttasjwi920@gmail.com>
2021-11-10 11:02:11 +0900

    구현이 완벽하지 않은 파일은 제거

--- Week #2/Day 06/Account Book/src/main/Application.java
+++ Week #2/Day 06/Account Book/src/main/Application.java
@@ -1,6 +1,17 @@
 package main;
 
+import util.Inputter;
+import java.util.Scanner;
+
 public class Application {
+
+    private final Scanner SC = Inputter.getInstance();
+    private static final String START_MENU_MSG =
+            "==============\n" +
+                    "1. 로그인\n" +
+                    "2. 사용자 등록\n" +
+                    "q. 프로그램 종료\n" +
+                    "=============";
 
     //TODO : Application 생성자 : 초기화 역할
     public Application() {
@@ -10,11 +21,7 @@ public class Application {
 
     //TODO : Application 실행
     public void run() {
-        System.out.println("명령을 입력해주세요.");
-        System.out.println("1. 로그인(미구현)");
-        System.out.println("2. 회원가입(미구현)");
-        System.out.println("q. 종료");
-
+        System.out.println(START_MENU_MSG);
         String command = inputCommand();
         if (command.equals("q")) {
             System.out.println("가계부 어플리케이션을 종료합니다. 바이바이~");
@@ -27,13 +34,34 @@ public class Application {
         2. 사용자로부터 입력을 받은 명령을 수행한다.
             만약 명령이 정해진 명령이 유효하지 않으면 다시 입력받도록 돌아간다.
          */
-        System.out.println("하와와. 뭔가가 잘못된것이에요");
+        System.out.println("뭔가 유효한 값이 제대로 넘어왔나보군! 예이~");
         }
 
 
     //TODO : 사용자로부터 명령을 입력받아 반환하도록 함.
     private String inputCommand() {
-        return "q";
+
+        System.out.println("값을 입력해주세요... (1, 2, q) >  ");
+        String input = SC.nextLine().trim().toLowerCase();
+        try {
+            inputValidCheck(input);
+        } catch (IllegalArgumentException ie) {
+            ie.printStackTrace();
+            return inputCommand();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return inputCommand();
+        }
+        return input;
+    }
+
+    private void inputValidCheck(String input) {
+        switch(input) {
+            case "1" : case "2" : case "q" :
+                return;
+            default : break;
+        }
+        throw new IllegalArgumentException("홈화면 입력 오류 : 유효한 입력값은 1, 2, q 입니다.");
     }
 
 }
--- Week #2/Day 06/Account Book/src/util/Resource.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-import java.util.Scanner;
-
-public class Resource {
-    static final Scanner scanner = new Scanner(System.in);
-}
