ac5965baf21fe14cd757dba8e318d4da0e0c7807
ttasjwi <ttasjwi920@gmail.com>
2021-11-15 23:06:52 +0900

    1초마다 한글시계가 실시간으로 갱신되도록 함

--- Week #3/Day 11/HangulClock/src/Main.java
+++ Week #3/Day 11/HangulClock/src/Main.java
@@ -3,7 +3,14 @@ import view.HangulClock;
 public class Main {
 
     public static void main(String[] args) {
-        HangulClock nowClock = HangulClock.now();
-        System.out.println(nowClock);
+        while (true) {
+            HangulClock nowClock = HangulClock.now();
+            System.out.println(nowClock);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
