6c3d7c35740e5f830ab8acc2100ce8e93ed06270
ttasjwi <ttasjwi920@gmail.com>
2021-12-01 16:32:31 +0900

    [Pokemon Calculator] Nature 클래스의 코드 가독성 개선 : 주석 추가, 일부 메서드명, 변수명 변경

--- Week #5/Pokemon Calculator/src/resource/Nature.java
+++ Week #5/Pokemon Calculator/src/resource/Nature.java
@@ -9,44 +9,11 @@ import java.util.*;
 
 public class Nature {
 
+    // NATURE_MAP의 데이터 소스 경로
+    private static final String DATA_SRC_DIR = "./src/resource/Nature_Table.txt";
+
+    // 모든 성격데이터 Map
     private static final Map<String, Nature> NATURE_MAP = initNatureMap();
-
-    //NATURE_MAP을 초기화한다.
-    private static Map<String,Nature> initNatureMap() {
-        List<String> lines = inputDataSrc();
-        HashMap<String, Nature> natureMap = new HashMap<>();
-        for (String line : lines) {
-            Nature nature = createNature(line);
-            natureMap.put(nature.getNatureName(), nature);
-        }
-        return natureMap;
-    }
-
-    //경로로부터 Nature 테이블 데이터를 가져온다.
-    private static List<String> inputDataSrc() {
-        List<String> lines = new ArrayList<>();
-        final Path DATA_SRC = Paths.get("./src/resource/Nature_Table.txt");
-        try {
-            Files.lines(DATA_SRC, Charset.defaultCharset()).forEach(lines::add);
-        } catch(IOException ie) {
-            System.out.println("성격데이터 로딩 실패!");
-        }
-        return lines;
-    }
-
-    // 읽은 라인으로부터 성격 데이터를 생성한다.
-    private static Nature createNature(String line) {
-        String[] lineSplit = line.split("\t");
-
-        String natureName = lineSplit[0];
-        double scale_Attack = Double.parseDouble(lineSplit[1]);
-        double scale_Block = Double.parseDouble(lineSplit[2]);
-        double scale_Contact = Double.parseDouble(lineSplit[3]);
-        double scale_Defense = Double.parseDouble(lineSplit[4]);
-        double scale_Speed = Double.parseDouble(lineSplit[5]);
-
-        return new Nature(natureName, scale_Attack, scale_Block, scale_Contact, scale_Defense, scale_Speed);
-    }
 
     private String natureName; // 성격명
     private double attackScale; // 공격 배율
@@ -55,6 +22,7 @@ public class Nature {
     private double defenseScale; // 특수방어 배율
     private double speedScale; // 스피드 배율
 
+    // 성격 생성자
     private Nature(String natureName, double attackScale, double blockScale, double contactScale, double defenseScale, double speedScale) {
         this.natureName = natureName;
         this.attackScale = attackScale;
@@ -64,38 +32,84 @@ public class Nature {
         this.speedScale = speedScale;
     }
 
+    //NATURE_MAP을 초기화 : 소스파일을 읽어와서, 내용을 기반으로 NATURE_MAP을 생성함.
+    private static Map<String,Nature> initNatureMap() {
+        List<String> lines = fileToLines(DATA_SRC_DIR);
+
+        Map<String, Nature> natureMap = new HashMap<>();
+        for (String line : lines) {
+            Nature nature = createNature(line);
+            natureMap.put(nature.getNatureName(), nature);
+        }
+        return natureMap;
+    }
+
+    //파일를 읽어와서, 라인단위로 분리한다.
+    private static List<String> fileToLines(String fileDir) {
+        List<String> tableSplits = new ArrayList<>();
+
+        try {
+            final Path DATA_SRC = Paths.get(fileDir);
+            Files.lines(DATA_SRC, Charset.defaultCharset())
+                    .forEach(tableSplits::add);
+        } catch(IOException ie) {
+            System.out.println("데이터 가져오기 실패!");
+        }
+        return tableSplits;
+    }
+
+    // 읽은 라인으로부터 성격 데이터를 생성한다.
+    private static Nature createNature(String line) {
+        String[] lineSplit = line.split("\t"); // 라인을 '\t'로 분리
+
+        String natureName = lineSplit[0];
+        double attackScale = Double.parseDouble(lineSplit[1]);
+        double blockScale = Double.parseDouble(lineSplit[2]);
+        double contactScale = Double.parseDouble(lineSplit[3]);
+        double defenseScale = Double.parseDouble(lineSplit[4]);
+        double speedScale = Double.parseDouble(lineSplit[5]);
+
+        return new Nature(natureName, attackScale, blockScale, contactScale, defenseScale, speedScale);
+    }
+
+    // 성격 문자열을 인자로 하여, NATURE_MAP에서 Nature 객체를 찾아온다.
     public static Nature of(String natureName) {
         return NATURE_MAP.get(natureName);
     }
 
+    // 모든 Nature 객체명이 담긴 Set을 반환한다.
     public static Set<String> nameSet() {
         return NATURE_MAP.keySet();
     }
 
+    // 성격명 반환
     public String getNatureName() {
         return natureName;
     }
 
+    // 공격 배율 반환
     public double getAttackScale() {
         return attackScale;
     }
 
+    // 방어 배율 반환
     public double getBlockScale() {
         return blockScale;
     }
 
+    // 특수공격 배율 반환
     public double getContactScale() {
         return contactScale;
     }
 
+    // 특수방어 배율 반환
     public double getDefenseScale() {
         return defenseScale;
     }
 
+    // 스피드 배율 반환
     public double getSpeedScale() {
         return speedScale;
     }
 
-    public static void main(String[] args) {
-    }
 }
